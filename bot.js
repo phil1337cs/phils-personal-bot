c142f6375768f67ff3d6ab938217786caa151775
phil1337cs <36329090+phil1337cs@users.noreply.github.com>
2018-02-10 13:47:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log('I am ready!');
+client.on('bereit', () => {
+  console.log('Ich bin bereit');
 });
 
 client.on('message', message => {
   if (message.content === '!bot') {
-    message.reply('Dieser Bot wurde von Philipp Kowalski am 10.02.2018 erstellt. Viel Spaß auf meinem Server!');
+    message.reply('Dieser Bot wurde von phil1337 am 10.02.2018 erstellt. Viel Spaß auf diesem Server!');
   }
 });
 
