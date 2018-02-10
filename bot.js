eb596f8bc05a732547cd78ebad1e220a844db86b
phil1337cs <36329090+phil1337cs@users.noreply.github.com>
2018-02-10 14:18:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,9 +14,9 @@ client.on('message', message => {
   {
     message.reply('\nDas Team NRW-United besitzt derzeit folgendes Lineup: \nphil1337 - IGL/Rifler\nweeaaks - Rifler (Support)\nJigga - Rifler\naimyo - Rifler (entry fragger)\nsmi. - AWPer\n\nProtyx - Rifler (Stand-In)\n\n\nLink zur Steamgruppe: http://steamcommunity.com/groups/nrw-utd \n\n\nLink zur 99-Damage Seite: https://csgo.99damage.de/de/leagues/teams/53733-nrw-united\n\n\nLink zur Facebook Seite: https://www.facebook.com/NRWunitedCSGO/') 
   }
-  else if (message.content === '!help')
+  else if (message.content === '!hilfe')
   {
-    message.reply('\n!bot = Infos über den Bot\n!nrwunited = Infos über unser Team\n!help = Liste der Commands')
+    message.reply('\n!bot = Infos über den Bot\n!nrwunited = Infos über unser Team\n!hilfe = Liste der Commands')
   }
                 
 });
