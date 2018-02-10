6809fde62d698438e45230b0a0854a15394fb14b
phil1337cs <36329090+phil1337cs@users.noreply.github.com>
2018-02-10 14:02:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,21 +6,14 @@ client.on('bereit', () => {
 });
 
 client.on('message', message => {
-  //if (message.content === '!bot') {
-    //message.reply('Dieser Bot wurde von phil1337 am 10.02.2018 erstellt. Viel Spaß auf diesem Server!');
-  //}
-  
-  if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
+  if (message.content === '!bot') 
+  {
+    message.reply('Dieser Bot wurde von phil1337 am 10.02.2018 erstellt. Viel Spaß auf diesem Server!');
+  }
+  else if (message.content === '!nrwunited')
+  {
+    message.reply('Das Team NRW-United besitzt derzeit folgendes Lineup: \nphil1337 - IGL/Rifler\nweeaaks - Rifler (Support)\nJigga - Rifler\naimyo - Rifler (entry fragger)\nsmi. - AWPer\n\nProtyx - Rifler (Stand-In)') 
+  }
                 
 });
 
